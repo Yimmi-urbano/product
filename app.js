d4c932e7172bd9f1e5f4e9aee74d89e145e239f6
Yimmi-urbano <139839538+Yimmi-urbano@users.noreply.github.com>
2024-05-14 15:18:28 -0500

    fix cors

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose'); // Importa mongoose aquí
 const bodyParser = require('body-parser');
-
+const cors = require('cors'); 
 const app = express();
 const PORT = 3000;
 
 app.use(bodyParser.json());
-
+app.use(cors);
 // Conexión a MongoDB
 mongoose.connect('mongodb+srv://data_user:wY1v50t8fX4lMA85@cluster0.entyyeb.mongodb.net/product', {
     useNewUrlParser: true,
