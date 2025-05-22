c323ed03c4943392dc22aabcf8ced5f8b2803573
Yimmi-urbano <yurbanoantunez@gmail.com>
2025-05-22 12:41:22 -0500

    fix: router products

--- api/products/products.router.js
+++ api/products/products.router.js
@@ -10,6 +10,7 @@ router.get('/search', validateDomain, controller.search);
 router.get('/category/:categorySlug', validateDomain, controller.byCategory);
 router.get('/:id', validateDomain, controller.getById);
 router.put('/:id', validateDomain, controller.update);
+router.patch('/:id', validateDomain, controller.update);
 router.delete('/:id', validateDomain,  controller.remove);
 router.get('/slug/:slug', validateDomain, controller.getProductBySlug);
 router.get('/client/:slug', validateDomain, controller.getProductBySlug);
